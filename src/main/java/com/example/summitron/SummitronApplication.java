326eead818050daaa3258587f00046b99974e86d
Grylls20 <piyushkmrstudent2003@gmail.com>
2025-07-08 11:53:38 +0530

    Commit 1: Project Scaffolding

---
+++ src/main/java/com/example/summitron/SummitronApplication.java
@@ -0,0 +1,13 @@
+package com.example.summitron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummitronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummitronApplication.class, args);
+    }
+
+}
